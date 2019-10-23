4fdef67f504e9b8956601cc9b8e1fe941ea7d05a
sylvain73 <sylvadavis73@gmail.com>
2019-10-23 14:47:01 +0200

    Update java.js

--- java.js
+++ java.js
@@ -6,26 +6,12 @@ output.innerHTML = slider.value;
 slider.oninput = function () {
   output.innerHTML = this.value;
 
+let montantDue;
 
-
-  if (this.value < 1500) {
-
-    var denominateur = (1 - (1 + (9.9 / 12) ^ (-6)));
-    var mensualite = (this.value * (9.9 / 12)) / denominateur;
-    console.log(mensualite);
-  }
-
-  if (this.value >= 1500 && this.value < 3000) {
-
-    var denominateur = (1 - (1 + (6 / 12) ^ (-6)));
-    var mensualite = (this.value * (6 / 12)) / denominateur;
-    console.log(mensualite);
-  }
-
-  if (this.value >= 3000 && this.value < 5000) {
-
-    var denominateur = (1 - (1 + (5.5 / 12) ^ (-6)));
-    var mensualite = (this.value * (5.5 / 12)) / denominateur;
+  if (this.value < 1500) 
+  {
+    
+   
     console.log(mensualite);
   }
 }
