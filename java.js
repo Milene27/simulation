ac720321f0b8cd6a7ba2ce3047a5bf1a7f7f8e23
milene <millerose22@gmail.com>
2019-10-23 15:29:46 +0200

    final

--- java.js
+++ java.js
@@ -28,7 +28,7 @@ var calcul=((this.value * taeg)/100);
   if (this.value >= 1500 && this.value < 3000) {
     var taeg = 6;
 
-    var calcul = ((this.value * 6) / 100);
+    var calcul = ((this.value * taeg) / 100);
     console.log("le intéret brut " + " " + calcul);
     montant = (parseFloat(this.value) + parseFloat(calcul));
     console.log(montant);
@@ -46,8 +46,9 @@ var calcul=((this.value * taeg)/100);
       console.log("la mensualité" + " " + result);
     }
 
-    document.getElementById("Mensualité").innerHTML = "Mensualité : " + result;
-    document.getElementById("montant").innerHTML = "Montant : " + montant;
+    document.getElementById("Mensualité").innerHTML = "Mensualité : " + result.toFixed(2);/* me permet de d'indiquer le montant de la valeur sur  ma  page HTML */
+    document.getElementById("montant").innerHTML = "Montant : " + montant.toFixed(2);
+    document.getElementById("TAEG").innerHTML = "taeg : " + taeg;
 
   }
 }
