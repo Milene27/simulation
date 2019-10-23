61d4c860482ce28f9bcd2b1eb5ef01d963695590
sylvain73 <sylvadavis73@gmail.com>
2019-10-23 15:25:50 +0200

    Changement

--- java.js
+++ java.js
@@ -3,19 +3,25 @@ var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
 
+
+
 slider.oninput = function () {
   output.innerHTML = this.value;
 
+var mois =12;
+var montant=0;
 
   if (this.value < 1500) {
-var calcul=((this.value*9.90)/100); 
-     
-    console.log("le intéret brut "+" "+calcul);
-   montant=(parseFloat(this.value) + parseFloat(calcul));
-    console.log(montant);
+        var calcul=((this.value*9.90)/100); 
+        var mensualite = document.getElementById("mensual");
 
-var result=(montant/mois);
-console.log("la mensualité"+" "+result);
+        montant=(parseFloat(this.value) + parseFloat(calcul));
+ 
+    var result=(montant/mois);
+    mensualite.innerHTML = result;
 
-
-  }}
+mensualite.oninput = function () {
+    mensualite.innerHTML = this.result;
+}
+  }
+}
