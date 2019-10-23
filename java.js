b46e26203418d99f60a178269b1254269ea4ecfe
jdc76 <jdcanchon@gmail.com>
2019-10-23 13:36:37 +0200

    fonction

--- java.js
+++ java.js
@@ -3,6 +3,30 @@ var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
 
-slider.oninput = function() {
+slider.oninput = function () {
   output.innerHTML = this.value;
+
+
+  if (this.value < 1500) {
+
+    var denominateur = (1 - (1 + (9.9 / 12) ^ (-6)));
+    var mensualite = (this.value * (9.9 / 12)) / denominateur;
+    console.log(mensualite);
+  }
+
+  if (this.value >= 1500 && this.value < 3000) {
+
+    var denominateur = (1 - (1 + (6 / 12) ^ (-6)));
+    var mensualite = (this.value * (6 / 12)) / denominateur;
+    console.log(mensualite);
+  }
+
+  if (this.value >= 3000 && this.value < 5000) {
+
+    var denominateur = (1 - (1 + (5.5 / 12) ^ (-6)));
+    var mensualite = (this.value * (5.5 / 12)) / denominateur;
+    console.log(mensualite);
+  }
 }
+
+
