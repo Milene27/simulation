ad40a6f0282a95bac9a808f23f4b8903a2af42c5
jdc76 <jdcanchon@gmail.com>
2019-10-23 15:24:12 +0200

    dd

--- java.js
+++ java.js
@@ -7,34 +7,34 @@ var montant = 0;
 slider.oninput = function () {
   output.innerHTML = this.value;
 
-  
-
-slider.oninput = function () {
-  output.innerHTML = this.value;
 
 
-  if (this.value < 1500) {
-    var calcul = ((this.value * 9.90) / 100);
-    console.log("le intéret brut " + " " + calcul);
-    montant = (parseFloat(this.value) + parseFloat(calcul));
-    console.log(montant);
-    var result = (montant / mois);
-    console.log("la mensualité" + " " + result);
+  slider.oninput = function () {
+    output.innerHTML = this.value;
 
-  }
 
-  if (this.value >= 1500 && this.value < 3000) {
-      
-    var calcul = ((this.value * 6) / 100);
-    console.log("le intéret brut " + " " + calcul);
-    montant = (parseFloat(this.value) + parseFloat(calcul));
-    console.log(montant);
-    var result = (montant / mois);
-    console.log("la mensualité" + " " + result);
-  }
+    if (this.value < 1500) {
+      var calcul = ((this.value * 9.90) / 100);
+      console.log("le intéret brut " + " " + calcul);
+      montant = (parseFloat(this.value) + parseFloat(calcul));
+      console.log(montant);
+      var result = (montant / mois);
+      console.log("la mensualité" + " " + result);
 
-  if (this.value >= 3000 && this.value < 5000) {
- 
+    }
+
+    if (this.value >= 1500 && this.value < 3000) {
+
+      var calcul = ((this.value * 6) / 100);
+      console.log("le intéret brut " + " " + calcul);
+      montant = (parseFloat(this.value) + parseFloat(calcul));
+      console.log(montant);
+      var result = (montant / mois);
+      console.log("la mensualité" + " " + result);
+    }
+
+    if (this.value >= 3000 && this.value < 5000) {
+
       var calcul = ((this.value * 5.5) / 100);
       console.log("le intéret brut " + " " + calcul);
       montant = (parseFloat(this.value) + parseFloat(calcul));
@@ -43,8 +43,4 @@ slider.oninput = function () {
       console.log("la mensualité" + " " + result);
     }
   }
-
-
-
-
-  }
+}
