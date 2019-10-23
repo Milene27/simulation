99f18caea346b6f070aed79742eb5568f07584f3
milene <millerose22@gmail.com>
2019-10-23 16:05:31 +0200

    der des der

--- java.js
+++ java.js
@@ -10,43 +10,45 @@ var montant = 0;
 slider.oninput = function () {
   output.innerHTML = this.value;
 
-var mois =12;
-var montant=0;
+  var mois = 12;
+  var montant = 0;
 
-  if (this.value < 1500) {/*"this.value me permet de récupérer le montant de la valeur de mon slider*/
-  var taeg = 9.90;
-  var calcul=((this.value * taeg)/100); 
-     
-    console.log("le intéret brut "+" "+calcul);
-   montant=(parseFloat(this.value) + parseFloat(calcul));
-  if (this.value < 1500) {
+/*"this.value me permet de récupérer le montant de la valeur de mon slider*/
+    var taeg = 9.90;
     var calcul = ((this.value * taeg) / 100);
+
     console.log("le intéret brut " + " " + calcul);
     montant = (parseFloat(this.value) + parseFloat(calcul));
-    console.log(montant);
-    var result = (montant / mois);
-    console.log("la mensualité" + " " + result);
-
-  }
-
-  if (this.value >= 1500 && this.value < 3000) {
-    var taeg = 6;
-
-    var calcul = ((this.value * taeg) / 100);
-    console.log("le intéret brut " + " " + calcul);
-    montant = (parseFloat(this.value) + parseFloat(calcul));
-    console.log(montant);
-    var result = (montant / mois);
-    console.log("la mensualité" + " " + result);
-  }
-
     if (this.value < 1500) {
-      var calcul = ((this.value * 9.90) / 100);
+      var calcul = ((this.value * taeg) / 100);
       console.log("le intéret brut " + " " + calcul);
       montant = (parseFloat(this.value) + parseFloat(calcul));
       console.log(montant);
       var result = (montant / mois);
       console.log("la mensualité" + " " + result);
+
+    }
+    else if (this.value >= 1500 && this.value < 3000) {
+      console.log("coucou");
+      var taeg = 6;
+
+      var calcul = ((this.value * taeg) / 100);
+
+      montant = (parseFloat(this.value) + parseFloat(calcul));
+
+      var result = (montant / mois);
+
+    }
+    else 
+
+{
+      var taeg = 5.5;
+      var calcul = ((this.value * taeg) / 100);
+
+      montant = (parseFloat(this.value) + parseFloat(calcul));
+
+      var result = (montant / mois);
+
 
     }
 
@@ -54,13 +56,8 @@ var montant=0;
     document.getElementById("montant").innerHTML = "Montant : " + montant.toFixed(2);
     document.getElementById("TAEG").innerHTML = "TAEG : " + taeg;
 
-      var calcul = ((this.value * 5.5) / 100);
-      console.log("le intéret brut " + " " + calcul);
-      montant = (parseFloat(this.value) + parseFloat(calcul));
-      console.log(montant);
-      var result = (montant / mois);
-      console.log("la mensualité" + " " + result);
-    }
+
   }
-}
+
+
 
