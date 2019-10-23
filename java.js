dc0cfb2f1b1c13afa995206f1047530453065a7b
milene <millerose22@gmail.com>
2019-10-23 14:59:57 +0200

    modif sylvain

--- java.js
+++ java.js
@@ -16,28 +16,7 @@ var calcul=((this.value*9.90)/100);
    montant=(parseFloat(this.value) + parseFloat(calcul));
     console.log(montant);
 var result=(montant/mois);
-console.log("lla mensualité"+" "+result);
-
-    var denominateur = (1 - Math.pow(1 + (9.9 / 12),(-6)));
-    var mensualite = (this.value * (9.9 / 12)) / denominateur;
-    console.log(mensualite);
-  }
-/*
-  if (this.value >= 1500 && this.value < 3000) {
-
-    var denominateur = (1 - Math.pow(1.5,-6));
-    var mensualite = (this.value * 0.5) / denominateur;
-    console.log(mensualite.toFixed(2));
-  }
-
-  if (this.value >= 3000 && this.value < 5000) {
-
-    var denominateur = (1 - (1 + (5.5 / 12) ^ (-6)));
-    var mensualite = (this.value * (5.5 / 12)) / denominateur;
-    console.log(mensualite);
-  }*/
-}
+console.log("la mensualité"+" "+result);
 
 
-
-
+  }}
